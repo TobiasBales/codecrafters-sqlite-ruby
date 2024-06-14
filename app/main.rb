eda9ae043dc845b8a7778a1012a0ac51f7320668
Tobias Bales <hi@tobiasbales.com>
2024-06-14 13:16:15 +0200

    Read write and read format from database file

--- app/main.rb
+++ app/main.rb
@@ -7,6 +7,10 @@ if command == ".dbinfo"
 
     database_file.seek(16)  # Skip the first 16 bytes of the header
     page_size = database_file.read(2).unpack("n")[0]
+    write_format = database_file.read(1).unpack("C")[0]
+    read_format = database_file.read(1).unpack("C")[0]
     puts "database page size: #{page_size}"
+    puts "write format: #{write_format}"
+    puts "read format: #{read_format}"
   end
 end
