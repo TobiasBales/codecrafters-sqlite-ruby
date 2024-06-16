1e11d207774b4cfb66415b9de1a5aa8d400f8da4
Tobias Bales <hi@tobiasbales.com>
2024-06-16 13:27:27 +0200

    Move db info into commands module

--- app/main.rb
+++ app/main.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 require_relative "buffer_backed"
-require_relative "db_info"
+require_relative "commands/db_info"
 require_relative "header"
 require_relative "reader"
 require_relative "page"
@@ -13,5 +13,5 @@ command = ARGV[1]
 if command == ".dbinfo"
   header, pages = Reader.read_file(database_file_path)
 
-  puts DbInfo.new(header, pages.first).generate
+  puts Commands::DbInfo.new(header, pages.first).generate
 end
