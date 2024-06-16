0681dc84ab5e58e035192ae72103b1cbc7acf8d8
Tobias Bales <hi@tobiasbales.com>
2024-06-16 19:15:21 +0200

    Add naive where clauses

--- app/queries/select.rb
+++ app/queries/select.rb
@@ -3,9 +3,10 @@
 
 module Queries
   class Select
-    def initialize(table, columns)
+    def initialize(table, columns, conditions)
       @table = table
       @columns = columns
+      @conditions = conditions
     end
 
     def execute(pages)
@@ -15,11 +16,22 @@ module Queries
       table_root_page = table_cell.record[:root_page]
 
       root_page = pages[table_root_page - 1]
-      root_page.cells.map do |cell|
+      root_page.cells.filter do |cell|
+        matches?(cell)
+      end.map do |cell|
         @columns.map do |column|
           cell.record[column]
         end.join("|")
       end
+    end
+
+    def matches?(cell)
+      return true unless @conditions
+
+      key = @conditions.first.to_sym
+      value = @conditions[1]
+
+      cell.record[key] == value
     end
   end
 end
--- app/query_parser.rb
+++ app/query_parser.rb
@@ -3,7 +3,7 @@
 
 class QueryParser
   SELECT_COUNT_REGEX = /^SELECT COUNT\(\*\) FROM (?<table>.*)$/i
-  SELECT_REGEX = /^SELECT (?<columns>[a-zA-Z\-_, ].+?) FROM (?<table>.*)$/i
+  SELECT_REGEX = /^SELECT (?<columns>[a-zA-Z\-_, ].+?) FROM (?<table>[a-zA-Z\-_]+)( WHERE (?<where>.*))?$/i
 
   class << self
     def parse(query)
@@ -11,8 +11,14 @@ class QueryParser
       when SELECT_COUNT_REGEX
         return Queries::Count.new(Regexp.last_match[:table])
       when SELECT_REGEX
+        table = Regexp.last_match[:table]
+
         columns = Regexp.last_match[:columns].split(",").map(&:strip).map(&:to_sym)
-        return Queries::Select.new(Regexp.last_match[:table], columns)
+
+        where = Regexp.last_match[:where]
+        conditions = where.split("=").map { |c| c.gsub("'", "").strip } if where
+
+        return Queries::Select.new(table, columns, conditions)
       else
         raise "Got unsupported query #{query}"
       end
